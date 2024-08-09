e364b3207a4599dfbd867b45936834f34421c7a1
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2024-08-09 15:16:48 +0530

    fixed type in removing digits

--- DP/Removing Digits.cpp
+++ DP/Removing Digits.cpp
@@ -9,7 +9,7 @@ const int mod = 1e9 + 7;
 void __solve() {
     int n; cin >> n;
     vector<int>dp (n + 1, n + 1);
-    dp[0] = 0; // minimum no of steps to make n 0 is 0
+    dp[0] = 0; // minimum no of steps to make 0 0 is 0
 
     for (int i = 1; i <= n; i++) {
         int num = i;
