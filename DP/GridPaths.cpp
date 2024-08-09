159e6a98095de3e15995e12ae66c0ddcf44a96df
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2024-08-09 15:26:27 +0530

    Update GridPaths.cpp

--- DP/GridPaths.cpp
+++ DP/GridPaths.cpp
@@ -1,43 +1,39 @@
+// author : md_nihal
 // https://cses.fi/problemset/task/1638
-// https://cses.fi/problemset/result/8813277/
-// Base Case : if i==n-1 and j==n-1 => 1
-//             else if i==n or j==n or a[i][s]=='*' => 0
-// dp[i]=will be dp[i+1][j] + dp[i][j+1] (moving down or moving right)
+https://cses.fi/problemset/result/10132938/
 
-#include "bits/stdc++.h"
+#include<bits/stdc++.h>
 using namespace std;
-#define all(x) x.begin(),x.end()
-#define ALL(x) x.rbegin(),x.rend()
-#define int long long
-const int mod=1e9+7,MAX=1e7+7,N=1e5+5;
- 
-#ifndef ONLINE_JUDGE
-#include "/home/anonymous/Templates/debug.hpp"
-#else
-#define debug(x...);
-#endif
-int dp[1005][1005],n;
-int helper(vector<string>&a,int i,int j){
-    if(i==n-1 and j==n-1 and a[i][j]!='*') return 1;
-    else if(i==n or j==n) return 0;
-    else if(a[i][j]=='*') return 0;
-    if(dp[i][j]!=-1) {
-        return dp[i][j];
+const int mod = 1e9 + 7;
+void __solve() {
+
+    int n; cin >> n;
+    vector<string>a (n);
+
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    vector<vector<int>>dp (n, vector<int> (n) );
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] == '*') continue;
+
+            if (i == 0 and j == 0) dp[i][j] = 1; // standing point there is only one way to reach here
+
+            if (i) dp[i][j] = dp[i - 1][j]; // no of ways to reach (i,j) from (i-1,j)
+
+            if (j) dp[i][j] += dp[i][j - 1]; // no of ways to reach (i,j) from (i,j-1)
+
+            dp[i][j] %= mod;
+        }
     }
-    int ans=0;
-    ans=(ans+helper(a,i+1,j))%mod;
-    ans=(ans+helper(a,i,j+1))%mod;
-    return dp[i][j]=ans;
+
+    cout << dp[n - 1][n - 1];
 }
-void __solve(){
-    cin>>n;
-    memset(dp,-1,sizeof dp);
-    vector<string>a(n);
-    for(auto &s:a) cin>>s;
-    cout<<helper(a,0,0);
+
+int main() {
+    int T = 1; //cin >> T;
+
+    while (T--) __solve();
 }
- 
-int32_t main(){
-    int T=1;  //cin>>T;
-    for(int i=0;i<T;i++,__solve(),cout<<endl);
-}
+
