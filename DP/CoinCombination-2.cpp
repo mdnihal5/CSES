629b1a14d423c84111dddaa9c890f35a8f935a77
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2024-08-09 15:07:08 +0530

    Update CoinCombination-2.cpp

--- DP/CoinCombination-2.cpp
+++ DP/CoinCombination-2.cpp
@@ -1,25 +1,37 @@
 // Author : md_nihal
 // https://cses.fi/problemset/result/8649864/
+// https://cses.fi/problemset/result/10132717/
 
 #include<bits/stdc++.h>
 using namespace std;
 
-const int mod=1e9+7;
-int main()
-{
-    int n,x; cin>>n>>x;
-    int arr[n+1];
-    for(int i=1;i<=n;i++) cin>>arr[i];
-    vector dp(n+1,vector(x+1,0));
-    for(int i=1;i<=n;i++){
-        for(int j=0;j<=x;j++){
-            if(j==0) {
-                dp[i][j]=1;
-            }else if(j-arr[i]>=0){
-                dp[i][j]=(dp[i-1][j]+dp[i][j-arr[i]])%mod;
-            }else dp[i][j]=dp[i-1][j];
+const int mod = 1e9 + 7;
+
+void __solve() {
+    int n, sum; cin >> n >> sum;
+
+    vector<int>a (n + 1);
+    vector<vector<int>>dp (n + 1, vector<int> (sum + 1) );
+
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+    for (int i = 0; i <= n; i++) dp[i][0] = 1; // no of ways to make target 0 is 1 for all i
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= sum; j++) {
+            dp[i][j] = dp[i - 1][j]; // no of ways to make target J using i-1 coins
+
+            if (j - a[i] >= 0) dp[i][j] += dp[i][j - a[i]]; // no of ways to make target J including ith coin
+
+            dp[i][j] %= mod;
         }
     }
 
-    cout<<dp[n][x]<<endl;
+    cout << dp[n][sum];
+}
+
+int main() {
+    int T = 1; //cin >> T;
+
+    while (T--) __solve();
 }
